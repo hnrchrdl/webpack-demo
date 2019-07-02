f269ca173efecd261c6e0040d22921cacb5e0e1f
hnrchrdl <hinrich@posteo.de>
2019-07-02 06:55:45 +0200

    asset management - wrapping up

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import _ from "lodash";
-import "./style.css";
-import webpackLogo from "./wp.png";
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
 
-  const wplogo = new Image();
-  wplogo.src = webpackLogo;
-
-  element.appendChild(wplogo);
-
-  console.log(Data);
 
   return element;
 }
